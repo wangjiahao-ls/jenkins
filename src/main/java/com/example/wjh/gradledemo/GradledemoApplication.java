c03477980b2a785f807c3ba2f79c5e5b7665b5cb
wangjiahao <m13350768179@163.com>
2020-02-12 21:00:23 +0800

    di

--- src/main/java/com/example/wjh/gradledemo/GradledemoApplication.java
+++ src/main/java/com/example/wjh/gradledemo/GradledemoApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import javafx.application.Application;
 
 @SpringBootApplication
 public class GradledemoApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+        return application.sources(GradledemoApplication.class);
     }
 
     public static void main(String[] args) {
